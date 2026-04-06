687553a70f7618a6823c15ca3e1087a8eae8b8d4
Dayna Blackwell <blackwellsystems@protonmail.com>
2026-04-05 17:03:46 -0700

    chore: gitignore rust target dir in test fixture

--- test/fixtures/rust/target/debug/deps/hello-163eccf9bf4de038.d
+++
@@ -1,5 +0,0 @@
-/Users/dayna.blackwell/code/lsp-mcp-improved/test/fixtures/rust/target/debug/deps/hello-163eccf9bf4de038.d: src/main.rs
-
-/Users/dayna.blackwell/code/lsp-mcp-improved/test/fixtures/rust/target/debug/deps/libhello-163eccf9bf4de038.rmeta: src/main.rs
-
-src/main.rs:
--- test/fixtures/rust/target/debug/deps/hello-f7ed93ecdcd3b228.d
+++
@@ -1,5 +0,0 @@
-/Users/dayna.blackwell/code/lsp-mcp-improved/test/fixtures/rust/target/debug/deps/hello-f7ed93ecdcd3b228.d: src/main.rs
-
-/Users/dayna.blackwell/code/lsp-mcp-improved/test/fixtures/rust/target/debug/deps/libhello-f7ed93ecdcd3b228.rmeta: src/main.rs
-
-src/main.rs:
